b98e8d99373b0137103caba737a38bfec17aeb88
Ahmad Sholikin <Ahmad Sholikin@DESKTOP-E6QG73J.local>
2021-10-12 11:20:04 +0700

    Update page-add.php and page-edit.php

---
+++ app/Views/backend/datatpp/nominalkelasjabatan/page-add.php
@@ -0,0 +1,8 @@
+<div class='card'>
+    <div class='card-header bg-transparent'>
+        <a href="">Formulir Tambah Data Baru Nominal Kelas Jabatan</a>
+    </div>
+    <div class='card-body'>
+        
+    </div>
+</div>
---
+++ app/Views/backend/datatpp/nominalkelasjabatan/page-edit.php
@@ -0,0 +1,8 @@
+<div class='card'>
+    <div class='card-header bg-transparent'>
+        <a href="">Formulir Edit Data Baru Nominal Kelas Jabatan</a>
+    </div>
+    <div class='card-body'>
+        
+    </div>
+</div>
