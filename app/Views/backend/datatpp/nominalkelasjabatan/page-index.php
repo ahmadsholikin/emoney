f5f519d562b4b8912622115f1d395c5f902edc44
Ahmad Sholikin <Ahmad Sholikin@DESKTOP-E6QG73J.local>
2021-10-12 11:17:45 +0700

    Update NominalKelasJabatan.php, NominatifPenerima.php, and 11 more files...

---
+++ app/Views/backend/datatpp/nominalkelasjabatan/page-index.php
@@ -0,0 +1,8 @@
+<div class='card'>
+    <div class='card-header bg-transparent'>
+        <a href="">Nominal Kelas Jabatan</a>
+    </div>
+    <div class='card-body'>
+        
+    </div>
+</div>
---
+++ app/Views/backend/datatpp/nominatifpenerima/page-index.php
@@ -0,0 +1,8 @@
+<div class='card'>
+    <div class='card-header bg-transparent'>
+        <a href="">Nominatif Penerima TPP</a>
+    </div>
+    <div class='card-body'>
+        
+    </div>
+</div>
